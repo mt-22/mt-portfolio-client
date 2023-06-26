812909219a086997d4c3872244d5631bea7d275d
mt-22 <marshalll.taylorrr@gmail.com>
2023-06-25 17:10:25 -0700

    hosting stuff

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -4,4 +4,6 @@ export default axios.create({
     withCredentials: true
 })
 
-export const hostURL = "https://web-production-f06d.up.railway.app/"
+// export const hostURL = "https://web-production-f06d.up.railway.app/"
+// export const hostURL = "http://10.0.0.214:5000/"
+export const hostURL = "https://mt-portfolio-api.onrender.com/"
