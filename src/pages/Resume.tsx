12c1a18ae54b01dd3a5cc7b45d8a9a2849c6daf4
mt-22 <39211762+mt-22@users.noreply.github.com>
2024-04-12 09:09:51 -0700

    Update Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,7 +4,7 @@ import Document from 'react-pdf'
 const Resume = () => {
   return (
     <div style={{height:"100vh", width:"100%", margin:0}}>
-        <iframe src="https://docs.google.com/gview?url=https://github.com/mt-22/mt-22.github.io/raw/main/mtaylor_resume.pdf&embedded=true"
+        <iframe src="https://docs.google.com/gview?url=https://github.com/mt-22/mt-22.github.io/raw/master/mtaylor_resume.pdf&embedded=true"
         style={{width:"100%", height:"100%"}}
         />
     </div>
